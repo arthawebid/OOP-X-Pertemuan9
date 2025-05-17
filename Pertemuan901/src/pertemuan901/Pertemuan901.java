62967f8bc077a467dbd1b0d591461cd640c5118d
OOP-X <arthawebid@gmail.com>
2025-05-17 17:02:34 +0800

    pertemuan 9

--- Pertemuan901/src/pertemuan901/Pertemuan901.java
+++ Pertemuan901/src/pertemuan901/Pertemuan901.java
@@ -1,20 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pertemuan901;
 
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
- * @author INSTIKI
+ * @author Made Artha
+ * TGL 17 Mei 2025
  */
 public class Pertemuan901 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String[] args) throws SQLException {
+        dbkoneksi dbk = new dbkoneksi();
+        
+        Statement stm = dbk.koneksi().createStatement();
+        String sql = "SELECT * FROM mhs;";
+        ResultSet rs = stm.executeQuery(sql);
+        
+        while( rs.next() ){
+            System.out.println("Nama : " + rs.getString("NAMA"));
+        }
     
-}
+}}
